317173465dcf38c7c36c59a5f008a0544a4c59dd
voin <voin@b580.com>
2017-04-08 20:50:00 +0300

    fix priority custom load module event

--- src/Module.php
+++ src/Module.php
@@ -33,23 +33,23 @@ class Module implements AutoloaderProviderInterface
     {
         $events = $moduleManager->getEventManager();
 
-        $events->attach(ModuleEvent::EVENT_LOAD_MODULE, array($this, 'onLoadModule'));
+        $events->attach(ModuleEvent::EVENT_LOAD_MODULE, array($this, 'onLoadModule'), 1);
     }
 
     /**
      * @param ModuleEvent $e
+     * @return ConfigListener
      */
     public function onLoadModule(ModuleEvent $e)
     {
         /** @var ConfigListener $configListener */
         $configListener = $e->getParam('configListener');
-        $configListener->onLoadModule($e);
 
         $modulePath = str_replace('\\', '/', $e->getModuleName());
 
         $path = 'module/' . $modulePath . '/config/autoload';
 
-        if(!is_dir($path)) return;
+        if(!is_dir($path)) return $configListener;
 
         foreach (new \DirectoryIterator(getcwd() . '/' . $path) as $fileInfo) {
             if ($fileInfo->isDot()) {
@@ -58,5 +58,7 @@ class Module implements AutoloaderProviderInterface
             $file = $path . '/' . $fileInfo->getFilename();
             $configListener->addConfigStaticPath($file);
         }
+
+        return $configListener;
     }
 }
